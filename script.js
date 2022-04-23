d2e52d0d7e24f799205019911ae98b64a7650eef
William Bradshaw <will.bradshaw50@gmail.com>
2022-04-23 18:36:45 +1000

    added sharable urls

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-function updateText () {
-    let inputText = prompt("Please enter some text", "Hello World!")
+inputText = "Hello World!"
+const url = new URL(window.location.href);
+
+function updateText() {
     document.getElementById("text").innerHTML = inputText;
-    document.getElementById("text").style.animationDuration = String((inputText.length)/2) + 's';
+    document.getElementById("text").style.animationDuration = String((inputText.length) / 2) + 's';
+    url.searchParams.set("text", inputText);
+    window.history.replaceState(null, null, url);
+}
+
+function pageLoad() {
+    urlLoad = url.searchParams.get("text")
+    
+    if (urlLoad != "") {
+        inputText = url.searchParams.get("text");
+    }
+
+    updateText();
+}
+
+function buttonClick() {
+    inputText = prompt("Please enter some text", "Hello World!");
+    updateText();
 }

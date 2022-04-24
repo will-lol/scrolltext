d41de4713f6ec66de5c41bc3e23a4c24919c389a
William Bradshaw <will.bradshaw50@gmail.com>
2022-04-24 10:48:12 +1000

    removed console logs

--- script.js
+++ script.js
@@ -4,14 +4,12 @@ const url = new URL(window.location.href);
 function updateText() {
     document.getElementById("text").innerHTML = inputText;
     document.getElementById("text").style.animationDuration = String((inputText.length) / 2) + 's';
-    console.log(btoa(inputText));
     url.searchParams.set("text", btoa(inputText));
     window.history.replaceState(null, null, url);
 }
 
 function pageLoad() {
     urlLoad = url.searchParams.get("text");
-    console.log(urlLoad);
     
     if (urlLoad == null) {
         url.searchParams.set("text", btoa("Hello World!"));
@@ -19,7 +17,6 @@ function pageLoad() {
     }
 
     inputText = atob(url.searchParams.get("text"));
-    console.log("Final is " + inputText);
     updateText();
 }
 

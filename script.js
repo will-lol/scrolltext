767311e7d48bc638ef1a267eb18cd860dacb0155
William Bradshaw <will.bradshaw50@gmail.com>
2022-04-24 11:36:47 +1000

    refactored javascript

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
-inputText = "";
 const url = new URL(window.location.href);
 
-function updateText() {
+function updateText(inputText) {
     document.getElementById("text").innerHTML = inputText;
     document.getElementById("text").style.animationDuration = String((inputText.length) / 2) + 's';
     url.searchParams.set("text", btoa(inputText));
     window.history.replaceState(null, null, url);
 }
 
-function pageLoad() {
-    urlLoad = url.searchParams.get("text");
-    
+function urlCheck(urlLoad) {    
     if (urlLoad == null) {
         url.searchParams.set("text", btoa("Hello World!"));
         window.history.replaceState(null, null, url);
     }
-
-    inputText = atob(url.searchParams.get("text"));
-    updateText();
 }
 
 function buttonClick() {
     inputText = prompt("Please enter some text", "Hello World!");
-    updateText();
+    updateText(inputText);
+}
+
+function pageLoad() {
+    urlCheck(url.searchParams.get("text"))
+    updateText(atob(url.searchParams.get("text")));
 }

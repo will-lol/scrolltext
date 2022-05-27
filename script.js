06240bdb540ff93e6f1be0ce23cc3a1f6131223a
William Bradshaw <will.bradshaw50@gmail.com>
2022-05-27 22:57:23 +1000

    refactored js and added new color changing button!

--- script.js
+++ script.js
@@ -1,25 +1,43 @@
 const url = new URL(window.location.href);
 
+let color = {id:"color", default:"#f80000", currentValue:""};
+let text = {id:"text", default:"Hello World!", currentValue:""};
+
 function updateText(inputText) {
-    document.getElementById("text").innerHTML = inputText;
-    document.getElementById("text").style.animationDuration = String((inputText.length) / 2) + 's';
-    url.searchParams.set("text", btoa(inputText));
+    document.getElementById(text.id).innerHTML = inputText;
+    document.getElementById(text.id).style.animationDuration = String((inputText.length) / 2) + 's';
+    url.searchParams.set(text.id, btoa(inputText));
     window.history.replaceState(null, null, url);
 }
 
-function urlCheck(urlLoad) {    
-    if (urlLoad == null) {
-        url.searchParams.set("text", btoa("Hello World!"));
-        window.history.replaceState(null, null, url);
-    }
+function updateColor(inputColor) {
+    document.getElementById(text.id).style.color = inputColor;
+    document.getElementById(text.id).style.textShadow = "0px 0px 40px " + inputColor;
+    url.searchParams.set(color.id, btoa(inputColor));
+    window.history.replaceState(null, null, url);
 }
 
-function buttonClick() {
-    inputText = prompt("Please enter some text", "Hello World!");
-    updateText(inputText);
+function urlCheck(...urlLoad) {
+    for (let i=0;i<urlLoad.length;i++) {
+        if (url.searchParams.get(urlLoad[i].id) == null) {
+            url.searchParams.set(urlLoad[i].id, btoa(urlLoad[i].default));
+        }
+    }
+    window.history.replaceState(null, null, url);
+}
+
+function changeText() {
+    text.currentValue = prompt("Please enter some text", text.default);
+    updateText(text.currentValue);
+}
+
+function changeColor() {
+    color.currentValue = prompt("Please enter a hex color code", color.default);
+    updateColor(color.currentValue);
 }
 
 function pageLoad() {
-    urlCheck(url.searchParams.get("text"))
-    updateText(atob(url.searchParams.get("text")));
+    urlCheck(color, text);
+    updateText(atob(url.searchParams.get(text.id)));
+    updateColor(atob(url.searchParams.get(color.id)));
 }
